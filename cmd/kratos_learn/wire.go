ec1042185eea27da249e4e7e7a6ce1bafb1f80a9
zhaojianhui <jianhui.zhao@sfygroup.com>
2024-10-18 14:46:38 +0800

    first commit

---
+++ cmd/kratos_learn/wire.go
@@ -0,0 +1,23 @@
+//go:build wireinject
+// +build wireinject
+
+// The build tag makes sure the stub is not built in the final build.
+
+package main
+
+import (
+	"kratos_learn/internal/biz"
+	"kratos_learn/internal/conf"
+	"kratos_learn/internal/data"
+	"kratos_learn/internal/server"
+	"kratos_learn/internal/service"
+
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+)
+
+// wireApp init kratos application.
+func wireApp(*conf.Server, *conf.Data, log.Logger) (*kratos.App, func(), error) {
+	panic(wire.Build(server.ProviderSet, data.ProviderSet, biz.ProviderSet, service.ProviderSet, newApp))
+}
