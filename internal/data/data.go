7446318170d9c57499075945d337deddc9da3314
zjh <zhaojianhui129@163.com>
2024-10-30 00:22:34 +0800

    feat(ent): data部分使用ent数据库orm

--- internal/data/data.go
+++ internal/data/data.go
@@ -1,24 +1,52 @@
 package data
 
 import (
+	"context"
 	"kratos_learn/internal/conf"
+	"kratos_learn/internal/data/ent"
+	"kratos_learn/internal/data/ent/migrate"
 
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/google/wire"
 )
 
 // ProviderSet is data providers.
-var ProviderSet = wire.NewSet(NewData, NewGreeterRepo)
+var ProviderSet = wire.NewSet(NewData, NewEntClient, NewGreeterRepo)
 
 // Data .
 type Data struct {
-	// TODO wrapped database client
+	db  *ent.Client
+	log *log.Helper
 }
 
 // NewData .
-func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
-	cleanup := func() {
-		log.NewHelper(logger).Info("closing the data resources")
+func NewData(entClient *ent.Client, logger log.Logger) (*Data, func(), error) {
+	log := log.NewHelper(log.With(logger, "module", "catalog-service/data"))
+	d := &Data{
+		db:  entClient,
+		log: log,
 	}
-	return &Data{}, cleanup, nil
+	cleanup := func() {
+		if err := d.db.Close(); err != nil {
+			log.Error(err)
+		}
+	}
+	return d, cleanup, nil
+}
+
+func NewEntClient(c *conf.Data, logger log.Logger) *ent.Client {
+	log := log.NewHelper(log.With(logger, "module", "catalog-service/data/ent"))
+
+	client, err := ent.Open(
+		c.Database.Driver,
+		c.Database.Source,
+	)
+	if err != nil {
+		log.Fatalf("failed opening connection to db: %v", err)
+	}
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background(), migrate.WithForeignKeys(false)); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+	return client
 }
